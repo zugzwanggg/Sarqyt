9265a864198bdb305229518d93c9b1c581e74a56
Amankos Ansar <zugzwanggg0@gmail.com>
2025-08-09 19:22:33 +0500

    added '/api' for apis

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,12 +1,12 @@
 import { api } from "../App"
 
 export async function telegramAuth(initData: string) {
-  const res = await api.post('/auth/telegram', { initData });
+  const res = await api.post('/api/auth/telegram', { initData });
   return res.data; 
 }
 
 export const getMe =async () => {
-  const res = await api.get('/user');
+  const res = await api.get('/api/user');
 
   return res.data;
 }
