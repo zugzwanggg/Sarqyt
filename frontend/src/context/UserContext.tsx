f637cd3c1c6cbed1aecc6c3943d8e9c2e14f90dc
Amankos Ansar <zugzwanggg0@gmail.com>
2025-08-20 10:15:32 +0500

    checking user data

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -2,9 +2,11 @@ import { createContext, useContext } from "react";
 import { useTelegramLogin } from "../hooks/useTelegramLogin";
 
 const UserContext = createContext<any>(null);
-
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useTelegramLogin();
+
+  console.log(user);
+  
   return (
     <UserContext.Provider value={{ user, loading }}>
       {children}
