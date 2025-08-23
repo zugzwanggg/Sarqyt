551cf790fea47bdf92901986f4ae322915c01651
Amankos Ansar <zugzwanggg0@gmail.com>
2025-08-23 23:15:17 +0500

    changing cancelled to canceled

--- frontend/src/components/OrderCard.tsx
+++ frontend/src/components/OrderCard.tsx
@@ -13,7 +13,7 @@ export default function OrderCard({ order }: { order: IOrder }) {
         return "bg-indigo-100 text-indigo-700";
       case "completed":
         return "bg-green-100 text-green-700";
-      case "cancelled":
+      case "canceled":
         return "bg-red-100 text-red-700";
       default:
         return "bg-gray-100 text-gray-700";
