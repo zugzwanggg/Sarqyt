eec516f87d8ed77d48614cbd4efd4528b5145596
Amankos Ansar <zugzwanggg0@gmail.com>
2025-08-23 22:45:01 +0500

    reservation routes

--- backend/routes/sarqytRoute.js
+++ backend/routes/sarqytRoute.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkAuth } from "../middleware/checkAuth.js";
-import { addSarqytToFavorites, getSarqytById, getSarqytCategories, getSarqytsByUsersCity, removeSarqytFromFavorites } from "../controllers/sarqytController.js";
+import { addSarqytToFavorites, cancelReservation, getSarqytById, getSarqytCategories, getSarqytsByUsersCity, removeSarqytFromFavorites, reserveSarqyt } from "../controllers/sarqytController.js";
 import { checkIsUserShopOwner } from "../middleware/checkIsUserShopOwner.js";
 export const sarqytRoute = Router();
 
@@ -10,3 +10,6 @@ sarqytRoute.post('/favorites/', checkAuth, addSarqytToFavorites);
 sarqytRoute.delete('/favorites/:sarqytId', checkAuth, removeSarqytFromFavorites);
 sarqytRoute.get('/categories', getSarqytCategories);
 sarqytRoute.post('/shops/:shopId/sarqyts/', checkAuth, checkIsUserShopOwner);
+
+sarqytRoute.post('/reserve', checkAuth, reserveSarqyt);
+sarqytRoute.patch('/reserve', checkAuth, cancelReservation);
