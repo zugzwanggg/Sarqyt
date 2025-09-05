687b13a931eb941f9578ff6124ddc5e850fbc4ab
Amankos Ansar <zugzwanggg0@gmail.com>
2025-09-05 18:36:43 +0500

    scan data api

--- backend/controllers/sellerController.js
+++ backend/controllers/sellerController.js
@@ -25,3 +25,41 @@ export const acceptOrder = async (req,res) => {
     res.status(500).json({ message: error.message });
   }
 }
+
+export const getScanData = async (req,res) => {
+  try {
+
+    const {id} = req.params;
+
+    if (!id) return res.status(404).json({
+      message: "Provide id value"
+    })
+    const checkOrder = await db.query("SELECT 1 FROM orders WHERE id = $1", []);
+
+    if (checkOrder.rows[0].length <= 0) {
+      return res.status(404).json({
+        message: "Order doesn't exist"
+      })
+    }
+
+    const order = await db.query(`
+      SELECT 
+        o.id AS order_id,
+        u.username,
+        pt.title AS product_name,
+        o.pickup_time,
+        o.status
+      FROM orders o
+      JOIN users u ON u.id = o.user_id
+      JOIN sarqyts s ON s.id = o.sarqyt_id
+      JOIN product_types pt ON pt.id = s.product_type_id
+      WHERE o.id = $1;
+    `)
+
+    res.status(200).json(order.rows[0]);
+    
+  } catch (error) {
+    console.error('Error at getScanData:', error);
+    res.status(500).json({ message: error.message });
+  }
+}
--- backend/routes/sellerRoute.js
+++ backend/routes/sellerRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { checkAuth } from "../middleware/checkAuth.js";
 import { checkIsUserShopOwner } from "../middleware/checkIsUserShopOwner.js";
-import { acceptOrder } from "../controllers/sellerController.js";
+import { acceptOrder, getScanData } from "../controllers/sellerController.js";
 
 
 export const sellerRouter = Router();
 
 sellerRouter.patch('/scan', checkAuth, checkIsUserShopOwner, acceptOrder);
+sellerRouter.get('/scan/:id', checkAuth, checkIsUserShopOwner, getScanData);
